e85926f577501af080272d888eb80ac6f966a8c7
Your Name <you@example.com>
2022-05-04 17:16:28 -0500

    Migraciones y materialize

--- routes/web.php
+++ routes/web.php
@@ -76,3 +76,8 @@ Route::get('paises' , function(){
 
            return view('paises')->with('paises',$paises); 
 });
+
+
+Route::get('prueba', function(){
+    return view('productos.create');
+});
